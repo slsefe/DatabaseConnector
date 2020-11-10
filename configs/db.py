24912101317002a5f4c3e90ab8ec7dde7c699625
Bai Yin <17693374267@163.com>
2020-11-10 18:24:30 +0800

    Create db.py

---
+++ configs/db.py
@@ -0,0 +1,23 @@
+HIVE_CONFIG = {
+    "host": '127.0.0.1',
+    "port": 10000,
+    "username": 'username',
+    "password": 'password'
+}
+
+MYSQL_CONFIG = {
+    'test':
+        {
+            "host": "127.0.0.1",
+            "port": 3306,
+            "user": "username",
+            "password": "password",
+            "database": "default"},
+    'prod':
+        {
+            "host": "127.0.0.1",
+            "port": 3306,
+            "user": "username",
+            "password": "password",
+            "database": "default"},
+}
